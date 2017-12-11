f78f59e33fbec9243d754c459c8f06eb19b2ee5c
afowowe olalekan <tosyne.eksu@gmail.com>
2017-12-11 07:59:14 +0100

    I have added my UI files

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //implementation
         btnHairdresserSignin = (Button) findViewById(R.id.btnHairDresserSignin);
         btnHairdresserSignin.setOnClickListener(new HairdresserSigninClickListener(this));
     }
